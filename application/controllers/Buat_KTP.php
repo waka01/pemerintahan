40bf4f12acf99b2a71e6fe980700ab89cc2bc282
Imam Ramadhani <38097395+waka01@users.noreply.github.com>
2018-11-10 16:17:29 +0700

    Add files via upload

---
+++ application/controllers/Buat_KTP.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+  class buat_ktp extends CI_Controller {
+    public function index()
+	{
+		$this->load->view('buat_ktp');
+	}
+}
